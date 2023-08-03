9c8e8a1275922a9b38c2ac30f7990cf5704d572d
unknown <tomekf.dev@gmail.com>
2023-08-03 09:23:37 +0200

    contact, about pages blueprints

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = "core"
 urlpatterns = [
     path("", views.homepage_view, name="homepage"),
+    path("about", views.about_view, name="about"),
+    path("contact", views.contact_view, name="contact"),
 ]
--- core/views.py
+++ core/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 
 def homepage_view(request):
-    return render(request, "core/home.html")
+    section = "homepage"
+    return render(request, "core/home.html", {"section": section})
+
+
+def about_view(request):
+    section = "about"
+    return render(request, "core/about.html", {"section": section})
+
+
+def contact_view(request):
+    section = "contact"
+    return render(request, "core/contact.html", {"section": section})
