279ffa2d0bea0940a12a8ab63eb2131a51de303c
unknown <tomekf.dev@gmail.com>
2023-08-16 15:48:40 +0200

    allauth email login

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -56,6 +56,10 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
 
 
 MIDDLEWARE = [
