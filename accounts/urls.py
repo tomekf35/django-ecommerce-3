162b99fff565e0af32b6c308708441692debdb49
unknown <tomekf.dev@gmail.com>
2023-08-31 11:54:12 +0200

    profile: profile, purchases

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from allauth.account.views import confirm_email
+from . import views
+
 
 urlpatterns = [
     path(
         "accounts/confirm-email/<str:key>/", confirm_email, name="account_confirm_email"
+    ),
+    path("profile/<str:username>/", views.user_profile_view, name="user_profile"),
+    path(
+        "profile/<str:username>/purchases",
+        views.user_purchases_view,
+        name="user_purchases",
     ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def user_profile_view(request, username):
+    return render(request, "profile/profile.html")
+
+
+@login_required
+def user_purchases_view(request, username):
+    return render(request, "profile/purchases.html")
