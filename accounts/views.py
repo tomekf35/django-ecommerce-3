ee4a566960abdb6dbfca3b2a1e017bcbb019db8b
unknown <tomekf.dev@gmail.com>
2023-09-01 12:10:11 +0200

    user profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import CustomUser
 
 
 @login_required
 def user_profile_view(request, username):
-    return render(request, "profile/profile.html")
+    user = get_object_or_404(CustomUser, username=username)
+    return render(request, "profile/profile.html", {"user": user})
 
 
 @login_required
